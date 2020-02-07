612e3ccb63bf54303d76da8a34c64f6a5888d3e9
Nick <ntrueb2@illinois.edu>
2020-02-07 16:16:18 -0600

    articles sorted out

--- main.py
+++ main.py
@@ -37,12 +37,33 @@ def word(type):
     word += ' '
     return word
 
+def concatenate(arr):
+    ret = ''
+    return ret.join(arr)
+
+def getArticle(index, sentence, offset):
+    print(index)
+    if(random.randint(0,1) == 0):
+        if(len(sentence) != 0):
+            firstChar = sentence[index][0]
+            if(firstChar == 'a' or firstChar == 'e' or firstChar == 'i' or firstChar == 'o' or firstChar == 'u'):
+                sentence.insert(index, articles[0 + offset]) # An
+            else:
+                sentence.insert(index, articles[1 + offset]) # A
+        else:
+            sentence.insert(index, articles[2 + offset]) # The
+    else:
+        sentence.insert(index, articles[2 + offset]) # The 
+
 #open files in read-only mode to load words
 nounFile = open("nouns.txt",'r')
 verbFile = open("verbs.txt",'r')
 adjFile = open("adj.txt", 'r')
 prepFile = open("prepositions.txt", 'r')
 
+articles = ["An ", "A ", "The ", "an ", "a ", "the "] #, "A ", "the ",  "a "]
+numArticles = 3
+conjunctions = ["and ", "but ", "or "]
 nouns = [] #0
 verbs = [] #1
 adjs = []  #2
@@ -68,18 +89,54 @@ for i in prepFile:
     preps.append(i.replace('\n', ''))
 prepFile.close()
 
+#make varying complexity sentences
+#nouns = [] #0
+#verbs = [] #1
+#adjs = []  #2
+#preps = [] #3
+sentence = []
+numAdj = random.randint(1,2)
+
+indexBeforeAdjs = len(sentence) - 1
+if(indexBeforeAdjs == -1):
+    indexBeforeAdjs = 0
+
+for i in range(numAdj):
+    sentence.append(word(2))
+
+#get correct article in beginning of sentence
+getArticle(indexBeforeAdjs,sentence,0)
+
+# add noun
+sentence.append(word(0))
+sentence.append(word(1))
+if(random.randint(0,1) == 1):
+    sentence.append(word(3))
+    # add preposition
+    indexBeforeAdjs = len(sentence) 
+
+    for i in range(numAdj -1):
+        sentence.append(word(2))
+    getArticle(indexBeforeAdjs, sentence, numArticles)
+    sentence.append(word(0))
+
+print(sentence)
+final = concatenate(sentence)
+final = final[:len(final)-1] + '.'
+print(final)
+
 #make output file and generate sentences
-output = open("output.txt", "w+")
-for _ in range(3):
-    paragraph = '   '
-    for i in range(10):
-        sentence = "The " + word(2) + word(0) + word(1) + word(3) + "the " + word(2) + word(0)
-        paragraph += sentence[:len(sentence)-1] + '. '
-        
-    print(paragraph)
-    print('')
-    output.write(paragraph + "\n\n")
-output.close()
+#output = open("output.txt", "w+")
+#for _ in range(3):
+#   paragraph = '   '
+#    for i in range(10):
+#        sentence = "The " + word(2) + word(0) + word(1) + word(3) + "the " + word(2) + word(0)
+#        paragraph += sentence[:len(sentence)-1] + '. '
+#        
+#    print(paragraph)
+#    print('')
+#    output.write(paragraph + "\n\n")
+#output.close()
 
 
 
